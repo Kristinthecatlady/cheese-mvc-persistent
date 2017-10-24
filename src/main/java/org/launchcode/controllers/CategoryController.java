955572104f17a4540597188649c5ca7e7f4941dd
Kristin Alarcon <kristin.m.alarcon@gmail.com>
2017-10-24 15:40:18 -0700

    Finished Part 2/ Video 2

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping
+@RequestMapping("category")
 public class CategoryController {
 
     @Autowired
